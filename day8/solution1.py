add62be54cefc6b3951e67d293d99c527fd37325
iMylene13 <iMylene13@gmail.com>
2021-12-08 09:23:37 +0100

    Cleaned up code

--- day8/solution1.py
+++ day8/solution1.py
@@ -1,16 +1,10 @@
 import sys
 
 data = [ x.split(" | ")[1].strip("\n").split() for x in sys.stdin.readlines() ]
-
-segment_one = 2
-segment_four = 4
-segment_seven = 3
-segment_eight = 7
-correct_segments = [segment_one, segment_four, segment_seven, segment_eight]
-
+segments = [2, 4, 3, 7]
 counter = 0
 for line in data:
     for pattern in line:
-        if len(pattern) in correct_segments:
+        if len(pattern) in segments:
             counter += 1
 print(counter)
